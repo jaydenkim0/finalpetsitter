03aef56cf2343c5263d560d37f69b0d66e1d698c
Jieun Kim <Jieun Kim@DESKTOP-QBSD1JQ>
2020-02-01 19:47:50 +0900

    Dto 생성 수정

--- petmily/src/main/java/com/kh/petmily/entity/PetsitterDto.java
+++ petmily/src/main/java/com/kh/petmily/entity/PetsitterDto.java
@@ -16,7 +16,6 @@ public class PetsitterDto {
 	private String info; // 소개글
 	private String sitter_pets; // 반려동물 여부 '예', '아니오' 만 등록가능
 	private String care_condition; // 위탁할 공간이 있는지 있으면 환경을 서술
-	private String sitter_status; // '정상' 또는 '휴면' 만등록가능
-	
+	private String sitter_status; // '정상' 또는 '휴면' 만등록가능	
 
 }
