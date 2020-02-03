9918b1e9add553f643e0fb61567ee5dbaa68175f
user1 <user1@DESKTOP-0IFG55Q>
2020-02-03 16:23:23 +0900

    펫시터 가입 mapper 구현

--- petmily/src/main/java/com/kh/petmily/controller/PetsitterController.java
+++ petmily/src/main/java/com/kh/petmily/controller/PetsitterController.java
@@ -1,10 +1,25 @@
 package com.kh.petmily.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.petmily.entity.PetsitterDto;
 
 @Controller
 @RequestMapping("/petsitter")
 public class PetsitterController {
-
+	
+	@GetMapping("/regist")
+	public String regist() {
+		return "petsitter/regist";
+	}
+	
+	@PostMapping("/regist")
+	public String regist(@ModelAttribute PetsitterDto petsitterDto) {
+		return "./";
+	}
+	
 }
