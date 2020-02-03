b95d0a323e6464c46e8f3c2424c550ecfff72fd7
user1 <user1@DESKTOP-0IFG55Q>
2020-02-03 13:46:06 +0900

    관리자 메인페이지 기능

--- petmily/src/main/java/com/kh/petmily/repository/AdminDao.java
+++ petmily/src/main/java/com/kh/petmily/repository/AdminDao.java
@@ -1,5 +1,20 @@
 package com.kh.petmily.repository;
 
+import java.util.List;
+
+import com.kh.petmily.entity.MemberDto;
+
 public interface AdminDao {
+
+	List<MemberDto> getMemberList(MemberDto memberDto);
+
+	int getMtotal();
+
+	int getPtotal();
+	
+	int getAtotal();
+
+	
+	
 
 }
