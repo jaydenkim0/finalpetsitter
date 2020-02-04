aebf5a1ea669f1de3b311c88bb7882db48edeeea
hangeumju <gxxmjx1017@naver.com>
2020-02-04 18:10:11 +0900

    공지게시판 완성 (네비게이터 빼고)

--- petmily/src/main/java/com/kh/petmily/controller/board/FaqController.java
+++ petmily/src/main/java/com/kh/petmily/controller/board/FaqController.java
@@ -5,19 +5,15 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.petmily.entity.FaqDto;
 import com.kh.petmily.service.board.FaqService;
-import com.kh.petmily.vo.FaqVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,46 +24,55 @@ public class FaqController {
 	@Autowired
 	FaqService faqService;
 	
-	//01_1. 작성
+	@Autowired
+	FaqDto faqDto;
+	
+	//게시글 목록
+	@GetMapping("/list")
+	public String list(Model model) throws Exception {
+		List<FaqDto>list = faqService.list();
+		model.addAttribute("list",list);
+		return "board/faq/list";
+	}
+	//게시글 작성
 	@GetMapping("/write")
-	public String write() throws Exception {
-		return "board/faq/write";
-		}
-	//01_2. 작성 처리
+	public String write() {
+		return"board/faq/write";
+	}
+	//게시글 작성 처리
 	@PostMapping("/insert")
-	public String insert(@ModelAttribute FaqVO faqVO) throws Exception{
-		faqService.create(faqVO);
-		return "rediret:list";
+	public String write(FaqDto faqDto)throws Exception{
+		faqService.write(faqDto);
+		return "redirect:list";
+	}
+	//게시글 상세보기
+	@GetMapping("/view")
+	public String view(@RequestParam int faq_no, Model model)throws Exception {
+		FaqDto view = faqService.view(faq_no); 
+		model.addAttribute("view",view);
+		return "board/faq/view";
+	}
+	//게시글 수정
+	@GetMapping("/update")
+	public String update(@RequestParam int faq_no, Model model)throws Exception{
+		//FaqDto view 내용을 수정
+		FaqDto view = faqService.view(faq_no); 
+		model.addAttribute("view",view);
+		return "board/faq/update";
 	}
 	
-	//작성
-//	public String write(FaqDto faqDto) throws Exception {
-//		log.info("write");
-//		faqService.write(faqDto);
-//		return "redirect:/";
-//		return "redirect:/list";
-//	}
+	//게시글 수정 처리
+	@PostMapping("/update")
+	public String update(FaqDto faqDto)throws Exception{
+		faqService.update(faqDto);
+		return "redirect:list";
+	}
 	
-	//02. 목록
-	@RequestMapping("/list")
-	public ModelAndView list() throws Exception{
-		List<FaqVO> list = faqService.listAll();
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("board/faq/list");
-		mav.addObject("list",list);
-		return mav;
-	}
-	//03. 글 수정
-	@PostMapping("update")
-	public String update(@ModelAttribute FaqVO faqVO) throws Exception{
-		faqService.update(faqVO);
-		return "redircet:list";
-	}
-	//04. 글 삭제
-	@PostMapping("delete")
-	public String delete(@RequestParam int faq_no) throws Exception{
+	//게시글 삭제 처리
+	@GetMapping("/delete")
+	public String delete(@RequestParam int faq_no)throws Exception{
 		faqService.delete(faq_no);
-		return "redirct:list";
+		return "redirect:list";
 	}
 }
 
--- petmily/src/main/java/com/kh/petmily/service/board/FaqService.java
+++ petmily/src/main/java/com/kh/petmily/service/board/FaqService.java
@@ -8,21 +8,14 @@ import com.kh.petmily.entity.FaqDto;
 import com.kh.petmily.vo.FaqVO;
 
 public interface FaqService {
-	//public void write(FaqDto faqDto)throws Exception;
-	
-	//글 작성
-	public void create(FaqVO faqVO) throws Exception;
-	
-	//게시글 전체 목록
-	public List<FaqVO> listAll() throws Exception;
-	
+	//게시글 조회
+	public List<FaqDto>list()throws Exception;
+	//게시글 작성 실행
+	public void write(FaqDto faqDto)throws Exception;
 	//게시글 상세보기
-	public FaqDto read(int faq_no) throws Exception;
-	
-	//게시글 수정
-	public void update(FaqVO faqVO) throws Exception;
-	
-	//게시글 삭제
+	public FaqDto view(int faq_no) throws Exception;
+	//게시글 수정 실행
+	public void update(FaqDto faqDto) throws Exception;
+	//게시글 삭제 실행
 	public void delete(int faq_no) throws Exception;
-
 }
