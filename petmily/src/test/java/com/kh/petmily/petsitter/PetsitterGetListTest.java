cd3a94acd1554d086776cadcc17051468e8da344
user1 <user1@DESKTOP-0IFG55Q>
2020-02-11 14:39:42 +0900

    펫시터 상세 조회 테스트

--- petmily/src/test/java/com/kh/petmily/petsitter/PetsitterGetListTest.java
+++ petmily/src/test/java/com/kh/petmily/petsitter/PetsitterGetListTest.java
@@ -10,7 +10,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.kh.petmily.vo.petsitter.PetsitterGetVO;
+import com.kh.petmily.entity.CareConditionDto;
+import com.kh.petmily.entity.CarePetTypeDto;
+import com.kh.petmily.entity.LocationDto;
+import com.kh.petmily.entity.SkillNameDto;
+import com.kh.petmily.vo.MemberPetsVO;
+import com.kh.petmily.vo.petsitter.PetsitterVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,10 +31,46 @@ public class PetsitterGetListTest {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@Test
+	//펫시터 기본 정보 조회
+//	@Test
 	public void getList() {
-		List<PetsitterGetVO> petsitterList = sqlSession.selectList("petsitter.regist", "aaaa");
+		List<PetsitterVO> petsitterList = sqlSession.selectList("petsitter.getList", "aaaa");
 		log.info("petsitter = {}", petsitterList);
+	}
+	
+	//펫시터 스킬 조회
+//	@Test
+	public void getSkillsList() {
+		List<SkillNameDto> skillsList = sqlSession.selectList("petsitter.getSkillsList", "aaaa");
+		log.info("skills = {}", skillsList);
+	}
+	
+	//돌봄 가능 동물 종류 조회
+//	@Test
+	public void getPetTypeList() {
+		List<CarePetTypeDto> petTypeList = sqlSession.selectList("petsitter.getPetTypeList", "aaaa");
+		log.info("petType = {}", petTypeList);
+	}
+	
+	//돌봄 환경 조회
+//	@Test
+	public void getCareConditionList() {
+		List<CareConditionDto> careConditionList = sqlSession.selectList("petsitter.getCareConditionList", "aaaa");
+		log.info("careCondition = {}", careConditionList);
+	}
+	
+	//활동 지역
+//	@Test
+	public void getLocationList() {
+		List<LocationDto> locationList = sqlSession.selectList("petsitter.getLocationList", "aaaa");
+		log.info("location = {}", locationList);
+	}
+	
+	//펫 조회
+	@Test
+	public void getPetList() {
+		List<MemberPetsVO> petList = sqlSession.selectList("petsitter.getPetList", "dahye2");
+		log.info("pet = {}", petList);
 	}
 
 }
